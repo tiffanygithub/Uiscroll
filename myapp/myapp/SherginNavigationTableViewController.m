4432e4a7b1eca588a3bc1c3f52e95d283a99039c
citetiffany <tiffany@tiffanyteki-iMac-2.local>
2014-05-30 15:56:23 +0800

    add  like Facebook
    add  like Facebook

---
+++ myapp/myapp/SherginNavigationTableViewController.m
@@ -0,0 +1,83 @@
+//
+//  SherginNavigationTableViewController.m
+//  SherginScrollableNavigationBarDemo
+//
+//  Created by Valentin Shergin on 31/03/14.
+//  Copyright (c) 2014 shergin research. All rights reserved.
+//
+
+#import "SherginNavigationTableViewController.h"
+
+#import "SherginScrollableNavigationBar.h"
+
+@interface SherginNavigationTableViewController ()
+
+@end
+
+@implementation SherginNavigationTableViewController
+
+int count = 30;
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+
+    // SherginScrollableNavigationBar
+    ((SherginScrollableNavigationBar *)self.navigationController.navigationBar).scrollView = self.tableView;
+    self.title = @"likefacebook";
+
+    
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    // SherginScrollableNavigationBar
+  //  ((SherginScrollableNavigationBar *)self.navigationController.navigationBar).scrollView = self.tableView;
+
+    //[super viewDidDisappear:animated];
+}
+
+- (void)lessItems {
+    count--;
+    [self refresh];
+}
+
+- (void)moreItems {
+    count++;
+    [self refresh];
+}
+
+- (void)refresh {
+    [self.tableView reloadData];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@""];
+    cell.textLabel.text = [NSString stringWithFormat:@"Cell #%d", (int)indexPath.row];
+    return cell;
+}
+
+@end
--- myapp/myapp/TableTamplateSecondViewController.m
+++ myapp/myapp/TableTamplateSecondViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "TableTamplateSecondViewController.h"
+#import "SherginScrollableNavigationBar.h"
+#import "SherginNavigationTableViewController.h"
 
 @interface TableTamplateSecondViewController ()
 
@@ -18,6 +20,20 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    UINavigationController *navigationController =
+    [[UINavigationController alloc] initWithNavigationBarClass:[SherginScrollableNavigationBar class] toolbarClass:nil];
+    
+    SherginNavigationTableViewController *tableViewController =
+    [[SherginNavigationTableViewController alloc] initWithStyle:UITableViewStylePlain];
+    
+    [navigationController setViewControllers:@[tableViewController] animated:NO];
+    
+    [self addChildViewController:navigationController];
+    [navigationController didMoveToParentViewController:self];
+    [self.view addSubview: navigationController.view];
+
 }
 
 - (void)didReceiveMemoryWarning
